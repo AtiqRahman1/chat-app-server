c7f890f2de8d9fb9e5afd590a498dfcb9af21943
Atiqur Rahman <atiq94.ar@gmail.com>
2022-08-03 21:33:12 +0600

    chat app server included

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messageRoute");
+const socket = require("socket.io")
+
 
 const app = express();
 require("dotenv").config();
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
-app.use("/api/auth", userRoutes)
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -23,4 +28,28 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const server= app.listen(process.env.PORT, ()=>{
     console.log(`server started on Port ${process.env.PORT}`)
-})
+});
+
+const io= socket(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        Credentials:true,
+    },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket)=>{
+    global.chatSocket = socket;
+    socket.on("add-user", (userId)=>{
+      onlineUsers.set(userId, socket.id);  
+    });
+
+    socket.on("send-msg", (data)=>{
+        const sendUserSocket = onlineUsers.get(data.to);
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    });
+    
+});
